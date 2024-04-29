c4ddefe42809ff1a4029e34cf0af191514adc117
Dipu <dipuchaurasiya91@gmail.com>
2024-04-29 18:23:36 +0545

    feat: signup validation added

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/immdipu/e-commerce-go/database"
 	"github.com/immdipu/e-commerce-go/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -17,6 +18,37 @@ func Signup(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		return
 	}
+
+	count, err := database.User.CountDocuments(c, bson.M{"email": user.Email})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "failed to check user",
+		})
+		return
+	}
+
+	if count > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "user already exists",
+		})
+		return
+	}
+
+	count, err = database.User.CountDocuments(c, bson.M{"phone": user.Phone})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "failed to check user",
+		})
+		return
+	}
+
+	if count > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "phone number already exists",
+		})
+		return
+	}
+
 	user.ID = primitive.NewObjectID()
 	user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 	user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
@@ -26,6 +58,7 @@ func Signup(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "failed to create new user",
 		})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
